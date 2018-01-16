94f031acca149771d555b03314429bc2304a5d5f
clementdroillard <droillard.clement@neuf.fr>
2018-01-16 21:50:32 +0100

    correctif

--- connexion.php
+++ connexion.php
@@ -1,5 +1,5 @@
  <?php
  	//connexion a notre base de donnee
- 	$dbh = new PDO('mysql:host=localhost;dbname=bbd_jeu_v1','root','rt2018');
+ 	$dbh = new PDO('mysql:host=localhost;dbname=bdd_jeu_v1','root','rt2018');
 
  ?>
